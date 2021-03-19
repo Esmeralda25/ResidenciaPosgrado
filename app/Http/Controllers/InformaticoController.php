fff004b228371a010f8eca35145bb0841f7a1790
Esmeralda25 <esmeraldaxdd@gmail.com>
2021-03-18 23:15:31 -0600

    Cree repositorio

---
+++ app/Http/Controllers/InformaticoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\informatico;
+use Illuminate\Http\Request;
+
+class InformaticoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\informatico  $informatico
+     * @return \Illuminate\Http\Response
+     */
+    public function show(informatico $informatico)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\informatico  $informatico
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(informatico $informatico)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\informatico  $informatico
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, informatico $informatico)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\informatico  $informatico
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(informatico $informatico)
+    {
+        //
+    }
+}
